6bc6924acdcf72e980fbe18e70614bdd88f08bc7
Senthil <senthilv@thoughtworks.com>
2011-12-31 21:58:41 +0530

    Argumetazied the input file name..

--- prez_maker.rb
+++ prez_maker.rb
@@ -6,13 +6,15 @@ def get_snippet(location)
  snippet_tag ? File.read(file_name).match(/--#{snippet_tag}--\n(?<snippet>.*)#--#{snippet_tag}--\n/m)[:snippet] : File.read(file_name)
 end
 
-file_to_parse = './sample_input.html'
-output_file = './sample_output.html'
-file_contents = File.read(file_to_parse)
+file_name_to_process = ARGV.shift
+p 'Give valid file name' and exit unless file_name_to_process && File.exist?(file_name_to_process)
+output_file = "#{file_name_to_process.chomp(File.extname(file_name_to_process))}_out.html"
+file_contents = File.read(file_name_to_process)
 file_contents.gsub!(/code:(.*)$/) do |match|
   "<pre class='brush: ruby;gutter: false;'>#{get_snippet($1.strip)}</pre>"
 end
 slides = file_contents.split(/^>>>>>>>>>.*\n/).collect{|raw_slide| RedCloth.new(raw_slide).to_html}
-slides.each{|s| p s}
 final_html = ERB.new(File.read('./template.html.erb')).result(binding)
 File.open(output_file,'w') {|f| f.write final_html}
+puts "Check #{output_file}"
+puts 'Done.'
